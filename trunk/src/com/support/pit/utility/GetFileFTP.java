be221090b904ce37ce7941235ee04e273caa1afd
buihai.nb.gv@gmail.com <buihai.nb.gv@gmail.com@59c88d4d-f12b-c216-14bf-92a4e70adbc1>
2013-01-03 07:19:35 +0000

    zip file ftp

--- trunk/src/com/support/pit/utility/GetFileFTP.java
+++ trunk/src/com/support/pit/utility/GetFileFTP.java
@@ -5,17 +5,21 @@
 package com.support.pit.utility;
 
 import com.support.pit.datatype.TreasuryPayment;
+import com.support.pit.paym.SupportTreasuryPaymApp;
+import com.support.pit.paym.SupportTreasuryPaymView;
+import com.support.pit.system.Message;
+import com.support.pit.ziper.FolderZiper;
+import java.io.FileNotFoundException;
 import java.net.SocketException;
 import org.apache.commons.net.ftp.FTPClient;
 import java.io.IOException;
 import java.io.FileOutputStream;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.logging.Level;
+import java.util.zip.ZipOutputStream;
 import org.apache.commons.lang.ArrayUtils;
 
-public class getFileFPT {
+public class GetFileFTP {
 
     public static void getFTP(ArrayList<TreasuryPayment> arr_tp_pit, ArrayList<TreasuryPayment> arr_tp_thieu, String fold_ftp, String excel_fld) {
         System.out.println("Loading files on ftp server ...please wait");
@@ -34,12 +38,12 @@ public class getFileFPT {
             int r = 0;
             String[] getToTranNo = new String[2];
             //Load and get Tran_No
-            for(String name: names){
+            for (String name : names) {
                 getToTranNo = name.split("_");
-                if(getToTranNo.length >= 2){
+                if (getToTranNo.length >= 2) {
                     tranNoToFind[r] = getToTranNo[1];
-                }                
-                r++;                
+                }
+                r++;
             }
 
             String file_name = "";
@@ -60,14 +64,12 @@ public class getFileFPT {
                     tp1.setMakb(arr_tp_thieu.get(i).getMakb());
                     tp1.setFilename(file_name);
                     arr_tp_thieu_co_bk.add(tp1);
-
-                } 
-                //add chứng từ ko tồn tại trong backup
+                } //add chứng từ ko tồn tại trong backup
                 else {
                     TreasuryPayment tp = new TreasuryPayment();
                     tp.setLcn_owner(arr_tp_thieu.get(i).getLcn_owner());
                     tp.setNgay_kb(arr_tp_thieu.get(i).getNgay_kb());
-                    tp.setTran_no(arr_tp_thieu.get(i).getTran_no());                    
+                    tp.setTran_no(arr_tp_thieu.get(i).getTran_no());
                     tp.setCqt(arr_tp_thieu.get(i).getCqt());
                     tp.setMa_cqthu(arr_tp_thieu.get(i).getMa_cqthu());
                     tp.setTen_cqt(arr_tp_thieu.get(i).getTen_cqt());
@@ -75,15 +77,15 @@ public class getFileFPT {
                     arr_tp_thieu_ko_bk.add(tp);
                     //System.out.println("no file ftp on backup, tran_no --->>> " + arr_tp_thieu.get(i).getTran_no());
                 }
-                System.out.println("check data on ftp to files "+i+"/"+total);
+                System.out.println("check data on ftp to files " + i + "/" + total);
             }
             System.out.println("get files from ftp success and start eport excel file");
 
 
 
 
-        } catch (IOException e_) {
-            e_.printStackTrace();
+        } catch (IOException e) {
+            SupportTreasuryPaymView.logger.log(Level.WARNING, Message.ERR_MESS_GET_FTP_FILE, e.getMessage());
         } finally {
             try {
                 if (fos != null) {
@@ -92,7 +94,7 @@ public class getFileFPT {
                 client.logout();
                 client.disconnect();
             } catch (IOException e) {
-                e.printStackTrace();
+                SupportTreasuryPaymView.logger.log(Level.WARNING, Message.ERR_MESS_GET_FTP_FILE, e.getMessage());
             }
         }
 
@@ -102,33 +104,29 @@ public class getFileFPT {
     }
 
     public static void findAndGetFTP(String file_copy, String fold_ftp) throws SocketException, IOException {
-        System.out.println("Loading files ftp server ...please wait");
-        FTPClient client = new FTPClient();
-
-        client.connect("10.64.85.28");
-        client.login("px1adm", "Gdt$2012");
-        client.changeWorkingDirectory("/DataPaym/Archive");
+        System.out.println("Loading files ftp server ... please wait");
+        FTPClient client = ftpConnection();
         String[] names = client.listNames();
         String[] tranNoToFind = new String[names.length];
-            int r = 0;
-            String[] getToTranNo = new String[2];
-            //Load and get Tran_No
-            for(String name: names){
-                getToTranNo = name.split("_");
-                if(getToTranNo.length >= 2){
-                    tranNoToFind[r] = getToTranNo[1];
-                }                
-                r++;                
+        int r = 0;
+        String[] getToTranNo = new String[2];
+        //Load and get Tran_No
+        for (String name : names) {
+            getToTranNo = name.split("_");
+            if (getToTranNo.length >= 2) {
+                tranNoToFind[r] = getToTranNo[1];
             }
+            r++;
+        }
         String file_name = "";
         FileOutputStream fos = null;
 
         int indexof = 0;
         int total = file_copy.split(",").length;
-        int i =0;
+        int i = 0;
         for (String curr_file : file_copy.split(",")) {
-            
-           
+
+
             indexof = ArrayUtils.indexOf(tranNoToFind, curr_file);
             if (indexof != -1) {
                 file_name = names[indexof];
@@ -138,16 +136,26 @@ public class getFileFPT {
 
             }
             i++;
-            System.out.println("check data on ftp to files "+i+"/"+total);
+            System.out.println("check data on ftp to files " + i + "/" + total);
         }
     }
 
-    public static void main(String[] args) throws SocketException, IOException {
+    /**
+     * Thực hiện kết nối ftp
+     * @return FTPClient
+     */
+    public static FTPClient ftpConnection() {
+        FTPClient client = new FTPClient();
+        try {
+            client.connect(SupportTreasuryPaymApp.prop.getProperty("ftp.url"));
+            client.login(SupportTreasuryPaymApp.prop.getProperty("ftp.user"), SupportTreasuryPaymApp.prop.getProperty("ftp.password"));
+            client.changeWorkingDirectory(SupportTreasuryPaymApp.prop.getProperty("ftp.WorkingDirectory"));
 
-        DateFormat df = new SimpleDateFormat("MMM/yyyy");
-        Date date = new Date();
-        System.out.println("01/" + df.format(date));
-       // txtCTDenNgay.setText(Utility.getMaxDateOnlinePaym(df.format(date)));
+        } catch (IOException e) {
+            SupportTreasuryPaymView.logger.log(Level.WARNING, Message.ERR_MESS_CONNECTION_FTP, e.getMessage());
+        }
+
+        return client;
 
     }
 }
