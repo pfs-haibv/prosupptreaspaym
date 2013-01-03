d54da85202df3c700c3b293f1cfba771c1573530
buihai.nb.gv <buihai.nb.gv@gmail.com>
2013-01-03 07:20:18 +0000

    zip file ftp

--- src/com/support/pit/paym/SupportTreasuryPaymApp.java
+++ src/com/support/pit/paym/SupportTreasuryPaymApp.java
@@ -3,11 +3,12 @@
  */
 package com.support.pit.paym;
 
+import com.support.pit.config.ConfigSupportTreasPaym;
+import com.support.pit.config.LoadMapDMuc;
 import com.support.pit.conn.ConnectDB;
-import java.io.IOException;
+import java.io.File;
 import java.sql.Connection;
-import java.sql.SQLException;
-import javax.swing.JOptionPane;
+import java.util.Properties;
 import org.jdesktop.application.Application;
 import org.jdesktop.application.SingleFrameApplication;
 
@@ -16,9 +17,10 @@ import org.jdesktop.application.SingleFrameApplication;
  */
 public class SupportTreasuryPaymApp extends SingleFrameApplication {
 
-        //Connection oracle
+    //Connection oracle
     public static Connection connORA = null;
-    
+    public static Properties prop = null;
+
     /**
      * At startup create and show the main frame of the application.
      */
@@ -47,15 +49,15 @@ public class SupportTreasuryPaymApp extends SingleFrameApplication {
     /**
      * Main method launching the application.
      */
-    public static void main(String[] args) throws IOException, SQLException {
-        try {
-            connORA = ConnectDB.getConnORA();
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null,ex.getMessage(),
-                                    "Lỗi kết nối CSDL",
-                                    JOptionPane.ERROR_MESSAGE);            
-    }
-        launch(SupportTreasuryPaymApp.class, args);
-
+    public static void main(String[] args) {
+        //load properties
+        prop = ConfigSupportTreasPaym.getProp();
+        //get connection
+        connORA = ConnectDB.getConnORA();
+        //load danh mục
+        LoadMapDMuc.loadDMucCQT(new File ("mapping/map_cqt.xml"));
+                
+        launch(SupportTreasuryPaymApp.class, args);   
+        
     }
 }
